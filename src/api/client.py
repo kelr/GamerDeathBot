b2325f9bce89e99e681bb1300ba1586b6edebcd7
kelr <kyle.ruan@protonmail.com>
2019-08-25 01:33:30 -0400

    Comments again

--- src/api/client.py
+++ src/api/client.py
@@ -7,11 +7,24 @@ class TwitchAPIClient():
     """Twitch API client."""
 
     def __init__(self, client_id, oauth_token):
-        """Initialize the API."""
+        """Initialize the API.
+
+        Args:
+            client_id -- client id
+            oauth_token -- super secret oauth token
+        """
         self._client_id = client_id
         self._oauth_token = oauth_token
 
     def channel_is_live(self, channel_id):
+        """Check if a channel is live by ID.
+        
+        Args:
+            channel_id -- integer ID to check
+
+        Return:
+            Boolean.
+        """
         params = {
             'stream_type': 'live',
         }
@@ -29,12 +42,19 @@ class TwitchAPIClient():
         return headers
 
     def _request_get(self, path, params=None):
-        """Perform a HTTP GET request."""
+        """Perform a HTTP GET request.
+        
+        Args:
+            path -- Path to append on default api path.
+            params -- Extra parameters to append on
+        Return:
+            JSON object response
+        """
         url = urljoin('https://api.twitch.tv/kraken/', path)
         headers = self._get_request_headers()
 
         response = requests.get(url, params=params, headers=headers)
-        
+
         if response.status_code >= 500:
             print("Got status " + str(response.status_code))
 
