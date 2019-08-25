41ab8fb0708924728d4971174cdbec311e83664a
kelr <kyle.ruan@protonmail.com>
2019-08-24 23:46:15 -0400

    comments

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ import random
 
 from cooldown import CommandCooldown
 from conn import SocketConnection
+import env
 
 # Timer threads
 CD_TIMERS = {
@@ -29,17 +30,27 @@ MSG_REGEX = re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
 MSG_GREETING = r"^(hi|hello|hey|yo|sup|greetings|) @*GamerDeathBot"
 
 def rx_thread(conn):
+    """Thread to read from the socket connection.
+    
+    Args:
+        conn -- SocketConnection object
+    """
     while True:
         response = conn.recv()
-        if response is None:
-            continue
-        elif response == "PING :tmi.twitch.tv":
-            # Handle ping pong, once ~5 mins
-            conn.send("PONG :tmi.twitch.tv")
-        else:
+        if response is not None:
             parse_msg(conn, response)
 
 def parse_msg(conn, msg):
+    """Thread to read from the socket connection.
+    
+    Args:
+        conn -- SocketConnection object
+    """
+    # Handle ping pong, once ~5 mins
+    if msg == "PING :tmi.twitch.tv":
+        conn.send("PONG :tmi.twitch.tv")
+        return
+
     username = re.search(r"\w+", msg).group(0)
     message = MSG_REGEX.sub("", msg)
     channel = re.search(r"#\w+", msg)
@@ -48,26 +59,58 @@ def parse_msg(conn, msg):
 
     print("RX: " + str(channel) + ":" + username + ":" + message.strip())
 
+    # Match a greeting message
     if (re.match(MSG_GREETING, message, re.IGNORECASE)):
         send_greeting(conn, channel, username)
 
+    # Match a gamerdeath message
     elif (message == "!gamerdeath"):
         send_gamerdeath(conn, channel)
 
 def send_greeting(conn, channel, username):
+    """Send a greeting message when someone says Hi to GDB.
+    
+    Args:
+        conn -- SocketConnection object
+        channel -- channel to reply to
+        username -- username to reply to
+    """
     if CD_TIMERS["greeting"].check_cooldown():
         conn.chat(channel, get_random_greeting(username))
         CD_TIMERS["greeting"].set_cooldown()
 
 def send_gamerdeath(conn, channel):
+    """Send a gamerdeath message when someone invokes !gamerdeath.
+    
+    Args:
+        conn -- SocketConnection object
+        channel -- channel to reply to
+    """
     if CD_TIMERS["gamerdeath"].check_cooldown():
         conn.chat(channel, "MrDestructoid Manual Gamer Death Prevention System activated! Please get up and prevent Gamer Death!")
         CD_TIMERS["gamerdeath"].set_cooldown()
 
 def get_random_greeting(username):
+    """Build a random greeting message.
+    
+    Args:
+        username -- username to reply to
+    """
     return random.sample(GREETS, 1)[0] + " " + username + " etalWave"
 
+def getup_thread(conn):
+    """Thread to tell the gamers to get up every 2 hours.
+
+    Args:
+        conn -- SocketConnection object
+    """
+    while True:
+        time.sleep(7200)
+        for chan in env.TARGET_CHANNELS:
+            conn.chat(chan, "MrDestructoid " + chan[1:] + " alert! It's been 2 hours and you should get up and stretch to prevent Gamer Death!")
+
 def main():
+    """Setup the socket connection and the rx thread."""
     conn = SocketConnection()
     conn.connect()
 
@@ -75,10 +118,13 @@ def main():
     rx_t.daemon = True
     rx_t.start()
 
+    getup_t = threading.Thread(target=getup_thread, args=(conn,))
+    getup_t.daemon = True
+    getup_t.start()
+
+    # Do nothing forever
     while True:
-        time.sleep(7200)
-        for chan in TARGET_CHANNELS:
-            chat(sock, chan, "MrDestructoid " + chan[1:] + " alert! It's been 2 hours and you should get up and stretch to prevent Gamer Death!")
+        time.sleep(1)
 
 if __name__ == '__main__':
     main()
--- conn.py
+++ conn.py
@@ -2,10 +2,18 @@
 
 import socket
 import time
+from threading import Lock
 
 import env
 
-def check_conn(func):
+def _check_conn(func):
+    """Decorator to check that a connection is established before doing anything
+    
+    Args:
+        func -- Function object to wrap
+    Returns:
+        Wrapped function
+    """
     def wrap(obj, *args, **kwargs):
         if obj.is_connected:
             return func(obj, *args, **kwargs)
@@ -14,12 +22,15 @@ def check_conn(func):
     return wrap
 
 class SocketConnection():
+    """Manages the socket connection"""
     def __init__(self):
+        """Constructor."""
         self._RX_BUF_SZ = 1024
         self._sock = None
         self.is_connected = False
 
     def connect(self):
+        """Initializes connection to the Twitch servers."""
         if not self.is_connected:
             self._sock = socket.socket()
             self._sock.connect((env.HOST, env.PORT))
@@ -30,22 +41,37 @@ class SocketConnection():
             for chan in env.TARGET_CHANNELS:
                 self.send("JOIN " + chan)
 
-    @check_conn
+    @_check_conn
     def send(self, msg):
+        """Send a message on the socket. Will attempt reconnection if socket conn is lost.
+    
+        Args:
+            msg -- string to send
+        """
         try:
-            self._sock.send((msg + "\r\n").encode("utf-8"))
+            self._sock.sendall((msg + "\r\n").encode("utf-8"))
         except Exception as e:
             print(e)
             self.is_connected = False
             time.sleep(1)
             self.connect()
 
-    @check_conn
+    @_check_conn
     def recv(self):
-        m = self._sock.recv(self._RX_BUF_SZ).decode("utf-8").strip()
-        return m
+        """Read data from the socket.
+
+        Returns:
+            stripped decoded string
+        """
+        return self._sock.recv(self._RX_BUF_SZ).decode("utf-8").strip()
 
     def chat(self, channel, msg):
+        """Send a chat message on a channel
+    
+        Args:
+            channel -- channel to send on
+            msg -- string to send
+        """
         m = "PRIVMSG " + channel + " :" + msg
         print("TX: " + m)
         self.send(m)
